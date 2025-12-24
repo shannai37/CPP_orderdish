6ab183b5e3ee8d3507d1491e22160cab278c74be
shannai37 <163226374+shannai37@users.noreply.github.com>
2025-12-24 19:49:47 +0800

    Delete project1117_homework_orderdish_class_instance directory

--- project1117_homework_orderdish_class_instance/last/CRegisterWin.h
+++
@@ -1,9 +0,0 @@
-#ifndef CREGISTER_H
-#define CREGISTER_H
-
-#include "CWindow.h"
-#include "CLoginWin.h"
-void CRegisterWin_init();
-
-int registerUser(char account[], char password[], char repassword[], char role[]); 
-#endif
